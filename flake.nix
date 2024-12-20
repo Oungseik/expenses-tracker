64a0289cd467da8ad9d71edc3f45dc723486a50e
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-12-21 06:16:02 +0630

    add biome as nix package to avoid ld problem

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+            python3
+            biome
+          ];
+
+          shellHook = '''';
+          LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+        };
+      }
+    );
+}
