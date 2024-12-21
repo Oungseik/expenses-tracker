3d1ca079e0379b84f1bf0176e0917cdb35ecd04b
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-12-21 12:53:49 +0630

    chore: change the module resolution to bunder to import without extension

--- packages/expenses-tracker-http/src/index.ts
+++ packages/expenses-tracker-http/src/index.ts
@@ -1,4 +1,4 @@
 import { HttpApi } from "@effect/platform";
-import { CheckHealthApi } from "./CheckHealth.js";
+import { CheckHealthApi } from "./CheckHealth";
 
 export const api = HttpApi.make("expensesTrackerApi").add(CheckHealthApi);
