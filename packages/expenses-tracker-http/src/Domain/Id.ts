3a838db71c93d1d8339ba5105c7a3eab8265fd62
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-12-25 15:54:22 +0630

    fix: add separator length in the id

--- packages/expenses-tracker-http/src/Domain/Id.ts
+++ packages/expenses-tracker-http/src/Domain/Id.ts
@@ -1,9 +1,9 @@
 import { Schema as S } from "effect";
 
 export const Id = S.String.pipe(
-  S.filter((s) => s.length === 32, { jsonSchema: { minLength: 32 } }),
+  S.filter((s) => s.length === 36, { jsonSchema: { minLength: 36 } }),
   S.brand("Id"),
-  S.annotations({ title: "Id", description: "An uuid v7 Id" }),
+  S.annotations({ title: "Id", description: "An uuid v7 Id with separator" }),
 );
 
 export type Id = typeof Id.Type;
