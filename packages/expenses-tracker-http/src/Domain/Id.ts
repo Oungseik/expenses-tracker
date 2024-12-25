9bd003a574a968a1a24dc2cc626e5ca7273318fe
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-12-25 13:25:16 +0630

    fix: add jsonSchema field to generate jsonSchema

--- packages/expenses-tracker-http/src/Domain/Id.ts
+++ packages/expenses-tracker-http/src/Domain/Id.ts
@@ -1,7 +1,7 @@
 import { Schema as S } from "effect";
 
 export const Id = S.String.pipe(
-  S.filter((s) => s.length === 32),
+  S.filter((s) => s.length === 32, { jsonSchema: { minLength: 32 } }),
   S.brand("Id"),
   S.annotations({ title: "Id", description: "An uuid v7 Id" }),
 );
